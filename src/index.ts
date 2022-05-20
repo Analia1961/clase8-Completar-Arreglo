7b18ecd7a80696a5ca458dd99c24fe698ebf267e
Analía Alvado <104107403+Analia1961@users.noreply.github.com>
2022-05-20 18:11:59 -0300

    clase8_ejercicio7 Se completó el ejercicio 7

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,29 @@
-import "./styles.css";
+// Arreglos (Ejercicios ONLINE) Viernes 13 de mayo de 2022
+// Ejercicio 7 - Completar Arreglo
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>`;
+/* 
+• Llenar un array de 10 posiciones 
+con números aleatorios entre 0 y 99.
+• Para obtener números aleatorios crear una función Azar,
+ que se base en las funciones disponibles en el paquete Math:
+-Math.random() devuelve un nro al azar entre 0 y 1.
+*/
 
+//EJERCICIO CREAR ARREGLO CON NUMEROS RAMDOM
+
+let indice: number;
+let arregloRandom: number[] = new Array(10);
+
+function Azar(tope: number): number {
+  return Math.floor(Math.random() * tope);
+}
+
+for (indice = 0; indice < 10; indice++) {
+  arregloRandom[indice] = Azar(100);
+}
+
+for (indice = 0; indice < 10; indice++) {
+  console.log(
+    "el numero en la posicion " + indice + " es " + arregloRandom[indice]
+  );
+}
